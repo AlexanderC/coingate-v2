16d7ae7fa5120838b2a5c77b828c6819bcc768d0
Peter Willis <peter.willis@cudoventures.com>
2019-03-20 16:08:10 +0000

    Add order checkout function

--- src/client/api.js
+++ src/client/api.js
@@ -1,4 +1,7 @@
 module.exports = {
+  async checkoutOrder(order_id, pay_currency) {
+    return { method: 'POST', entity: `orders/${ order_id }/checkout`, data: { pay_currency } };
+  },
   async createOrder({
     order_id, price_amount, price_currency,
     receive_currency, title, description,
@@ -10,8 +13,8 @@ module.exports = {
       callback_url, cancel_url, success_url, token
     }};
   },
-  async getOrder(orderId) {
-    return { method: 'GET', entity: `orders/${ orderId }` };
+  async getOrder(order_id) {
+    return { method: 'GET', entity: `orders/${ order_id }` };
   },
   async listOrders({ per_page = 100, page = 1, sort = 'created_at_desc' } = {}) {
     return { method: 'GET', entity: 'orders', params: { per_page, page, sort } };
